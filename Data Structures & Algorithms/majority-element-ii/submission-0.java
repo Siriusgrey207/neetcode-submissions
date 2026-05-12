f7dc7632713691bbcb6c9a0e5c2fe054ee86bdba
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-05-12 13:41:22 +0100

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        if (nums.length == 1) return Arrays.asList(nums[0]);
+        
+        // *number in nums* : *frequency* map
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // For each frequency in the map we test if the num appears at least 1/3 of the length of nums
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int num = entry.getKey();
+            int numFreq = entry.getValue();
+
+            double relativeFreq = (double) numFreq / nums.length;
+            if (relativeFreq > 0.3334) result.add(num);
+        }
+
+        return result;
+    }
+}
