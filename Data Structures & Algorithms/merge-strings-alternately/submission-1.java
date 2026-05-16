5278f5cc85920c57631f382b2d0b3239a9fca6f5
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-05-16 19:43:37 +0100

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder("");
+        int ptr1 = 0;
+        int ptr2 = 0;
+
+        while (ptr1 < word1.length() && ptr2 < word2.length()) {
+            sb.append(word1.charAt(ptr1));
+            sb.append(word2.charAt(ptr2));
+            ptr1++;
+            ptr2++;
+        }
+
+        while (ptr1 < word1.length()) {
+            sb.append(word1.charAt(ptr1));
+            ptr1++;
+        }
+
+        while (ptr2 < word2.length()) {
+            sb.append(word2.charAt(ptr2));
+            ptr2++;
+        }
+
+        return sb.toString();
+    }
+}
