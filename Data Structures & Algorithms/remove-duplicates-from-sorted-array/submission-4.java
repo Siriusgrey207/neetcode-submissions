ba727f378940f9e01432580a8eadf5a524a405a1
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-05-19 11:16:30 +0100

    Add: remove-duplicates-from-sorted-array - submission-4

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // Initialise a slow and a fast pointer.
+        int slow = 0;
+        int fast = 0;
+
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                nums[slow + 1] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow + 1;
+    }
+}
