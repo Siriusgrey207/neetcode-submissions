1258fb32f2d9eae885e4d5dc68c074b496526507
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-05-26 15:14:34 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        // Create two character maps of the strings
+        Map<Character, Integer> mapOfS = new HashMap<>();
+        Map<Character, Integer> mapOfT = new HashMap<>();
+
+        // Loop through the two strings separately and record the character counts.
+        for (Character c : s.toCharArray()) {
+            mapOfS.put(c, mapOfS.getOrDefault(c, 0) + 1);
+        }
+
+        for (Character c : t.toCharArray()) {
+            mapOfT.put(c, mapOfT.getOrDefault(c, 0) + 1);
+        }
+
+        return mapOfS.equals(mapOfT);
+    }
+}
