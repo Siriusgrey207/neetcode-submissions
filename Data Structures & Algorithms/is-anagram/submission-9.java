a654e7781fef1c21f7d6457a27e95f2304fb9ffd
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-06-01 23:30:18 +0100

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] freqs = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freqs[s.charAt(i) - 'a']++;
+            freqs[t.charAt(i) - 'a']--;
+        }
+
+        for (int val : freqs) {
+            if (val != 0) return false;
+        }
+
+        return true;
+    }
+}
+
+// Solution with a similar time and space complexity but more overhead:
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) return false;
+
+//         // Create two character maps of the strings
+//         Map<Character, Integer> countS = new HashMap<>();
+//         Map<Character, Integer> countT = new HashMap<>();
+
+//         // Loop through the two strings separately and record the character counts.
+//         for (int i = 0; i < s.length(); i++) {
+//             countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+//             countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+//         }
+
+//         return countS.equals(countT);
+//     }
+// }
