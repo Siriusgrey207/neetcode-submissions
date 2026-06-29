1222acc1570d66b769a97e047c9ea36692dc5a7d
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-06-29 14:06:28 +0100

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] pairs = new int[position.length][2];
+
+        // Initialise the pairs array of arrays with position and speed information.
+        for (int i = 0; i < position.length; i++) {
+            pairs[i][0] = position[i];
+            pairs[i][1] = speed[i];
+        }
+
+        // Sort the pairs based on position.
+        Arrays.sort(pairs, (a, b) -> Integer.compare(b[0], a[0]));
+        Stack<Double> stack = new Stack<>();
+
+        for (int[] pair: pairs) {
+            stack.push((double) (target - pair[0]) / pair[1]);
+            if (stack.size() >= 2 && stack.peek() <= stack.get(stack.size() - 2)) {
+                stack.pop();
+            }
+        }
+
+        return stack.size();
+    }
+}
