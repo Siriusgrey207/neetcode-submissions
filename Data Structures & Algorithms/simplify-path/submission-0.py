f62945dfeb27f0e5bbc108a1b3c2c2a968622d49
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-07-03 21:57:43 +0100

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        current = ""
+
+        for char in path + "/":
+            if char == "/":
+                if current == "..":
+                    if stack: stack.pop()
+
+                elif current != "" and current != ".":
+                    stack.append(current)
+
+                current = ""
+            
+            else:
+                current += char
+
+        return "/" + "/".join(stack)
