5def372607c85777293e7a9f73f00cf189f6aac1
Siriusgrey207 <73255574+Siriusgrey207@users.noreply.github.com>
2026-08-12 11:10:49 +0100

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles: number[], h: number): number {
+        // Two pointers that represent the range of possible bananas / hour eaten.
+        let left: number = 1;
+        let right: number = Math.max(...piles);
+        let res = right;
+
+        while (left <= right) {
+            const k: number = Math.floor(left + (right - left) / 2);
+            let totalTime = 0;
+
+            for (const pile of piles) {
+                totalTime += Math.ceil(pile / k);
+            }   
+
+            if (totalTime <= h) {
+                res = k;
+                right = k - 1;
+            } else {
+                left = k + 1;
+            }
+        }
+
+        return res;
+    }
+}
